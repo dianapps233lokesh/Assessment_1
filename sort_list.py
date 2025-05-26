2339041e944808c6dbb309a1b11ed6bdb8fe912d
lokesh <lokesh.saini@dianapps.com>
2025-05-26 15:29:56 +0530

    sorting implemented

--- sort_list.py
+++ sort_list.py
@@ -1,2 +1,10 @@
 def sort_ls(nums):
-    pass
+    n=len(nums)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if nums[j]>nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j]
+    return nums
+
+
+print(sort_ls([9, 5, 1, 4, 8, 7, 2, 6, 3]))
