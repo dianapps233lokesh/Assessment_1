36f8de39e48e11138502632e9f5f42a36e356418
lokesh <lokesh.saini@dianapps.com>
2025-05-26 15:22:55 +0530

    flatten list corrected

--- flatten_list.py
+++ flatten_list.py
@@ -1,11 +1,11 @@
-def flatten_ls(nums):
-    ans=[]
+def flatten_ls(nums,ans):
+    # ans=[]
     for num in nums:
         if isinstance(num,list):
-            ans.append(flatten_ls(num))
+            flatten_ls(num,ans)
         else:
             ans.append(num)
-    # return ans
-    print(ans)
+    return ans
+    # print(ans)
 
-print(flatten_ls([1,[2,[3,[4,[5,[6,[7]]]]]]]))
+print(flatten_ls([1,[2,[3,[4,[5,[6,[7]]]]]]],[]))
